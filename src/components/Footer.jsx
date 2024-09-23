ea919574e09156dc8bf51c98037ce12b5b865d63
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-09-23 16:04:46 +0300

    heder, hero, footer to scss code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Footer.css';
+import './Footer.scss';
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = ({ theme }) => {
